7bf50a7a26d1b8de80e25686253594e65c4725ef
Junru Shao <junrushao1994@gmail.com>
2022-06-27 09:00:40 -0700

    add accurate error reporting

--- python/tvm/script/parse/evaluator.py
+++ python/tvm/script/parse/evaluator.py
@@ -16,12 +16,103 @@
 # under the License.
 """AST Evaluation"""
 import ast
-from typing import Any, Dict, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, Optional, Union
 
 from . import doc
 
+if TYPE_CHECKING:
+    from .parser import Parser
+
+
+class ExprEvaluator:
+
+    parser: "Parser"
+    value_table: Dict[str, Any]
+    new_value_count: int
+
+    def __init__(self, parser: "Parser", value_table: Dict[str, Any]) -> None:
+        super().__init__()
+        self.parser = parser
+        self.value_table = value_table
+        self.new_value_count = 0
+
+    @staticmethod
+    def eval(parser: "Parser", value_table: Dict[str, Any], node: doc.AST) -> Any:
+        self = ExprEvaluator(parser, value_table)
+        result = self._visit(node)
+        if isinstance(result, doc.Name):
+            if result.id not in self.value_table:
+                self.parser.report_error(result, "Undefined variable: %s" % result.id)
+            return self.value_table[result.id]
+        if isinstance(result, doc.Constant):
+            return result.value
+        raise TypeError("Unexpected result type: %s" % type(result))
+
+    def _visit(self, node: doc.AST) -> Any:
+        if isinstance(node, list):
+            return [self._visit(n) for n in node]
+        if isinstance(node, tuple):
+            return tuple(self._visit(n) for n in node)
+        assert isinstance(node, doc.AST)
+        if isinstance(node, doc.Name):
+            if node.id not in self.value_table:
+                self.parser.report_error(node, "Undefined variable: %s" % node.id)
+            return node
+        if isinstance(node, doc.Constant):
+            return node
+        new_fields = {}
+        for field in node.__class__._FIELDS:  # pylint: disable=protected-access
+            if isinstance(field, doc.AST):
+                new_fields[field] = self._visit(getattr(node, field))
+            else:
+                new_fields[field] = getattr(node, field)
+        try:
+            new_value = _eval_expr(node.__class__(**new_fields), self.value_table)
+        except Exception as e:
+            self.parser.report_error(node, str(e))
+        else:
+            name = f"__tvm_tmp_value_{self.new_value_count}"
+            self.new_value_count += 1
+            self.value_table[name] = new_value
+            return doc.Name(
+                id=name,
+                ctx=doc.Load(
+                    lineno=node.lineno,
+                    col_offset=node.col_offset,
+                    end_lineno=node.end_lineno,
+                    end_col_offset=node.end_col_offset,
+                ),
+                lineno=node.lineno,
+                col_offset=node.col_offset,
+                end_lineno=node.end_lineno,
+                end_col_offset=node.end_col_offset,
+            )
+
 
 def eval_expr(
+    parser: "Parser",
+    node: Union[doc.expr, doc.Expression],
+    dict_globals: Optional[Dict[str, Any]],
+) -> Any:
+    value_table = {}
+    if dict_globals is not None:
+        value_table.update(dict_globals)
+    return ExprEvaluator.eval(parser, value_table, node)
+
+
+def eval_assign(
+    parser: "Parser",
+    target: doc.expr,
+    source: Any,
+) -> Dict[str, Any]:
+    try:
+        return _eval_assign(target, source)
+    except Exception as e:
+        parser.report_error(target, "Failed to evaluate assignment: %s" % str(e))
+        raise
+
+
+def _eval_expr(
     node: Union[doc.expr, doc.Expression],
     dict_globals: Optional[Dict[str, Any]],
 ) -> Any:
@@ -36,7 +127,7 @@ def eval_expr(
     return eval(exe, dict_globals)  # pylint: disable=eval-used
 
 
-def eval_assign(
+def _eval_assign(
     target: doc.expr,
     source: Any,
 ) -> Dict[str, Any]:
